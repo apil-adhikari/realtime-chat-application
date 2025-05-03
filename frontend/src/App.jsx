3944db98c4f57b026746e126eee41e30d95b4948
Apil Adhikari <apiladhikari.social@gmail.com>
2025-05-03 22:21:00 +0545

    Frontent Setup: added react-router for handling frontent routing, daisyUI component library, and react-hot-toast for toast notification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Route, Routes } from "react-router";
+import Homepage from "./pages/HomePage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationPage from "./pages/NotificationPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import { Toaster } from "react-hot-toast";
+const App = () => {
+    return (
+        <div className="h-screen" data-theme="night">
+            {/* SETTING UP OUR ROUTES */}
 
-export default App
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/notifications" element={<NotificationPage />} />
+                <Route path="/onboarding" element={<OnboardingPage />} />
+                <Route path="/call" element={<CallPage />} />
+                <Route path="/chat" element={<ChatPage />} />
+            </Routes>
+            <Toaster />
+        </div>
+    );
+};
+
+export default App;
