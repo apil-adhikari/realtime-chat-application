fd6078d78d7b83a85344eef70d919c53484465dd
Apil Adhikari <apiladhikari.social@gmail.com>
2025-05-04 17:55:04 +0545

    Added tanstack query for state management and axiox for fetching data - did short introduction of tanstack react query. - planed to use axiox instead of fetch. - seprated axiox instance configuration to include baseUrl and to allow sending cookies to backend with request using 'withCredentials: true'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,11 +11,55 @@ import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 
 import { Toaster } from "react-hot-toast";
+
+import { axiosInstance } from "./lib/axios.js";
+import { useQuery } from "@tanstack/react-query";
+
+// import { useQuery } from "@tanstack/react-query";
+// import axios from "axios";
+
 const App = () => {
+    // tanstack query basic tutorial
+    // query & mutations
+    // mutations: used with post,put,patch & delete requests
+    // query: used with get requests
+
+    // const { data, isLoading, error } = useQuery({
+    //     queryKey: ["todos"],
+    //     queryFn: async () => {
+    //         // Using FETCH
+    //         // const res = await fetch(
+    //         //     "https://jsonplaceholder.typicode.com/todos"
+    //         // );
+
+    //         // const data = await res.json();
+
+    //         // Using AXIOS
+    //         const res = await axios.get(
+    //             "https://jsonplaceholder.typicode.com/todos"
+    //         );
+    //         return res.data;
+    //     },
+    // });
+
+    // console.log("DATA:", { data });
+    // console.log({ isLoading });
+    // console.log({ error });
+
+    const { data, isLoading, error } = useQuery({
+        queryKey: ["user"],
+        queryFn: async () => {
+            const res = await axiosInstance.get("/users/me");
+            return res.data;
+        },
+        retry: false,
+    });
+
+    console.log(data);
+
     return (
         <div className="h-screen" data-theme="night">
             {/* SETTING UP OUR ROUTES */}
-
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/signup" element={<SignupPage />} />
